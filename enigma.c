aad491c8d69ba3eea09b0f475ce77012680a99e1
Leonardo Silveira <sombriks@gmail.com>
2017-01-19 12:14:38 -0300

    versão C funcionando. focar nos slides agora

--- enigma.c
+++ enigma.c
@@ -8,28 +8,32 @@ const int letter_limit = 51;
 
 int no_debug = 0;
 
-struct wheel {
+struct wheel
+{
   int letter_order[51];
   char letters[51];
   int enc_position;
   int dec_position;
 };
 
-int charToNumber(char c){
+int charToNumber(char c)
+{
   int v = -1;
   int i = letter_limit;
-  while(i--)
-    if(c == key_order[i])
+  while (i--)
+    if (c == key_order[i])
       v = i;
-    return v;
+  return v;
 }
 
-struct wheel makeWheel(char *order,int len){
+struct wheel makeWheel(char *order, int len)
+{
   struct wheel wh;
-  for(int i = 0 ; i < len ; i++){
+  for (int i = 0; i < len; i++)
+  {
     wh.letter_order[i] = charToNumber(order[i]);
     wh.letters[i] = order[i];
-  }  
+  }
   return wh;
 }
 
@@ -37,7 +41,8 @@ struct wheel wheels[4];
 
 const int total_wheels = 4;
 
-struct form {
+struct form
+{
   char one;
   char two;
   char three;
@@ -47,218 +52,297 @@ struct form {
 
 struct form f;
 
-int validate(char no) {
+int validate(char no)
+{
   no = tolower(no); // ctype.h
   int nu = charToNumber(no);
-  if (nu > letter_limit - 1) nu = letter_limit - 1;
-  else if (nu < 0) nu = 0;
+  if (nu > letter_limit - 1)
+    nu = letter_limit - 1;
+  else if (nu < 0)
+    nu = 0;
   return nu;
 }
 
 /************ getInputs() :: Find the user set variables from forms ************/
-void getInputs() {
+void getInputs()
+{
   //Wheel starts at given positions
   wheels[0].enc_position = validate(f.one);
   wheels[1].enc_position = validate(f.two);
   wheels[2].enc_position = validate(f.three);
   wheels[3].enc_position = validate(f.four);
-  
+
   wheels[0].dec_position = validate(f.one);
   wheels[1].dec_position = validate(f.two);
   wheels[2].dec_position = validate(f.three);
   wheels[3].dec_position = validate(f.four);
-  
-  if (f.debug) no_debug = 0;
-  else no_debug = 1;
+
+  if (f.debug)
+    no_debug = 0;
+  else
+    no_debug = 1;
 }
 
-char *toLowerCase(char *input, int len) {
-  
+char *toLowerCase(char *input, int len)
+{
+
   char *ret = malloc(len * sizeof(char));
-  
-  while(len--){
+
+  while (len--)
+  {
     ret[len] = tolower(input[len]);
   }
-  
+
   return ret;
 }
 
-struct txt {
+struct txt
+{
   char *value;
 };
 
-struct txt enigmalog; 
+struct txt enigmalog;
 
-void info(int op, char *msg) {
-  // TODO implement 
+void info(int op, char *msg)
+{
+  // TODO implement
+  printf(msg);
 }
 
-char numberToChar(int b) {
+char numberToChar(int b)
+{
   char a = '*';
-  if(b >= 0 && b < letter_limit -1)
+  if (b >= 0 && b < letter_limit - 1)
     a = key_order[b];
   return a;
 }
 
 //Encrytes the Message given as parameter
-char *Encryptor(char *raw_input_text,int input_len) {
-    
-  // setup wheels 
+char *Encryptor(char *raw_input_text, int input_len)
+{
+
+  // setup wheels
   getInputs();
-    
+
   // input_text = input_text.toLowerCase(); //Convert to lower case
-  char *input_text = toLowerCase(input_text,input_len);
+  char *input_text = toLowerCase(raw_input_text, input_len);
   int l = input_len;
-    
-  char c;
+
   char *output_text = malloc(input_len * sizeof(char));
-  
-  for (int i = 0; i < l; i++) {
-    c = input_text[i];
+
+  for (int i = 0; i < l; i++)
+  {
+    char c = input_text[i];
     int letters_number = charToNumber(c); //Get the numarical value
     int pos = letters_number;
-    
-    if (letters_number == -1) c = '*'; //If the inputed char is not presente in the alphabet
-    else {
-      if (!no_debug) {
+
+    if (letters_number == -1)
+      c = '*'; //If the inputed char is not presente in the alphabet
+    else
+    {
+      if (!no_debug)
+      {
         char buf[14];
-        sprintf(buf,"Char: %c,No:%d ->",c,letters_number);
-        info(2, buf);// "Char:" + c + ",No:" + fill(letters_number) + "->");
-      }//To this for all the wheels
-      for (int k = total_wheels - 1; k >= 0; k--) {
+        sprintf(buf, "\nChar: %c,No:%d ->", c, letters_number);
+        info(2, buf); // "Char:" + c + ",No:" + fill(letters_number) + "->");
+      }               //To this for all the wheels
+      for (int k = total_wheels - 1; k >= 0; k--)
+      {
         pos = pos + wheels[k].enc_position; //Turns the wheel to its 'enc_position'
-        if (pos >= letter_limit) pos = pos - letter_limit; //Makes the corrections. The wheel is a circle. So if it is after 25 it must be corrected
-        if (k > 0) pos = wheels[k].letter_order[pos]; //Finds the number at 'pos' and give it for the next wheel. This is needed for all wheel execpt the last (0'th) Wheel
-        if (!no_debug) {
+        if (pos >= letter_limit)
+          pos = pos - letter_limit; //Makes the corrections. The wheel is a circle. So if it is after 25 it must be corrected
+        if (k > 0)
+          pos = wheels[k].letter_order[pos]; //Finds the number at 'pos' and give it for the next wheel. This is needed for all wheel execpt the last (0'th) Wheel
+        if (!no_debug)
+        {
           char buf[25];
-          sprintf(buf,"| %d (%d - %c) in W %d",pos,wheels[k].enc_position,wheels[k].letters[pos],k);
-          info(2,buf);
-//           info(2, "|" + fill(pos) + "(" + fill(wheels[k].enc_position) + "-" + wheels[k].letters[pos] + ") in W " + k);
+          sprintf(buf, "| %d (%d - %c) in W %d", pos, wheels[k].enc_position, wheels[k].letters[pos], k);
+          info(2, buf);
+          //           info(2, "|" + fill(pos) + "(" + fill(wheels[k].enc_position) + "-" + wheels[k].letters[pos] + ") in W " + k);
         }
-      }      
+      }
       c = numberToChar(wheels[0].letter_order[pos]);
-      if (!no_debug) {
+      if (!no_debug)
+      {
         char buf[5];
-        sprintf(buf," -> %c",c);
+        sprintf(buf, " -> %c", c);
         info(0, buf);
       }
     }
     wheels[0].enc_position++; //Turns the wheel one time
-    
-    for (int k = 0; k < total_wheels; k++) {
+
+    for (int k = 0; k < total_wheels; k++)
+    {
       if (wheels[k].enc_position > letter_limit) //One full turn of any wheel is completed
       {
         wheels[k].enc_position = 0;
-        if (k + 1 != total_wheels) wheels[k + 1].enc_position++; //Do this if the current wheel is not the last wheel
+        if (k + 1 != total_wheels)
+          wheels[k + 1].enc_position++; //Do this if the current wheel is not the last wheel
       }
     }
-    
-//     output_text = output_text + c; //Get the final result, letter by letter
-    output_text[i]=c;
+
+    //     output_text = output_text + c; //Get the final result, letter by letter
+    output_text[i] = c;
   }
-  if (!no_debug) {
+  if (!no_debug)
+  {
     char buf[100];
-    sprintf(buf,"The Encrypted text is \"%s\"\n",output_text);
+    sprintf(buf, "\nThe Encrypted text is \"%s\"\n", output_text);
     info(0, buf);
   }
   return output_text;
 }
 
 //Decrypts the Message
-char *Decryptor(char *raw_input_text,int input_len) {
-  
-  // setup wheels 
+char *Decryptor(char *raw_input_text, int input_len)
+{
+
+  // setup wheels
   getInputs();
-  
-  char *input_text = toLowerCase(raw_input_text,input_len);
+
+  char *input_text = toLowerCase(raw_input_text, input_len);
   int l = input_len;
-  
-  char c;
+
   char *output_text = malloc(input_len * sizeof(char));
-  
-  for (int j = 0; j < l; j++) {
-    c = input_text[j];
-    
+
+  for (int j = 0; j < l; j++)
+  {
+    char c = input_text[j];
+
     char decrypt = ' ';
     int to = -1;
     int from = charToNumber(c); //Gets the 'from' of the encrypted charecter
     int ch = from;
-    if (!no_debug) {      
+    if (!no_debug)
+    {
       char buf[14];
-      sprintf(buf,"Char: %c,No:%d ->",c,from);
-      info(2, buf);// info(2, "Char:" + c + ",No:" + fill(from) + "->")
+      sprintf(buf, "\nChar: %c,No:%d ->", c, from);
+      info(2, buf); // info(2, "Char:" + c + ",No:" + fill(from) + "->")
     }
-    for (int k = 0; k < total_wheels; k++) {
+    for (int k = 0; k < total_wheels; k++)
+    {
       for (int i = 0; i < letter_limit; i++) //Go thru every letter in the wheel,
-        if (wheels[k].letter_order[i] == ch) { ch = i; break; } //Searching the char 'c'(as a number) in the 'letter_order'. If found, the position is sorted in 'ch'.
-        
-        ch = ch - wheels[k].dec_position;	//Adjusts the wheel position
-        if (ch < 0) ch = ch + letter_limit; //If the given 'to' is before a, find its original char from end
-        if (!no_debug) {
-//           info(2, "|" + fill(ch) + "(" + fill(wheels[k].dec_position) + "-" + wheels[k].letters[ch] + ") in W " + k);
-          char buf[25];
-          sprintf(buf,"| %d (%d - %c) in W %d",ch,wheels[k].dec_position,wheels[k].letters[ch],k);
-          info(2,buf);
-          
-        }
+        if (wheels[k].letter_order[i] == ch)
+        {
+          ch = i;
+          break;
+        } //Searching the char 'c'(as a number) in the 'letter_order'. If found, the position is sorted in 'ch'.
+
+      ch = ch - wheels[k].dec_position; //Adjusts the wheel position
+      if (ch < 0)
+        ch = ch + letter_limit; //If the given 'to' is before a, find its original char from end
+      if (!no_debug)
+      {
+        //           info(2, "|" + fill(ch) + "(" + fill(wheels[k].dec_position) + "-" + wheels[k].letters[ch] + ") in W " + k);
+        char buf[25];
+        sprintf(buf, "| %d (%d - %c) in W %d", ch, wheels[k].dec_position, wheels[k].letters[ch], k);
+        info(2, buf);
+      }
     }
     to = ch;
     wheels[0].dec_position++; //Turns the wheel one step
-    
+
     decrypt = numberToChar(to); //Find the letter in 'to'
-    if (c == '*') decrypt = '*'; //If the inputed text is a non-recoganizable charector, output '*'
-    if (!no_debug) {
+    if (c == '*')
+      decrypt = '*'; //If the inputed text is a non-recoganizable charector, output '*'
+    if (!no_debug)
+    {
       //       info(0, " -> " + decrypt);
       char buf[5];
-      sprintf(buf," -> %c",decrypt);
-      info(0, buf);      
+      sprintf(buf, " -> %c", decrypt);
+      info(0, buf);
     }
-    for (int k = 0; k < total_wheels; k++) {
-      if (wheels[k].dec_position > letter_limit)	//If dec_position exeeds the limit, start at top
+    for (int k = 0; k < total_wheels; k++)
+    {
+      if (wheels[k].dec_position > letter_limit) //If dec_position exeeds the limit, start at top
       {
         wheels[k].dec_position = 0;
-        if (k + 1 != total_wheels) wheels[k + 1].dec_position++; //Do this if the current wheel is not the last wheel
+        if (k + 1 != total_wheels)
+          wheels[k + 1].dec_position++; //Do this if the current wheel is not the last wheel
       }
     }
     output_text[j] = decrypt;
   }
-  if (!no_debug) {
-//     info(0, "The Decrypted Text is \"" + output_text + "\"");
-//     console.log(txt.value);
-//     txt.value = "";
+  if (!no_debug)
+  {
+    //     info(0, "The Decrypted Text is \"" + output_text + "\"");
+    //     console.log(txt.value);
+    //     txt.value = "";
     char buf[100];
-    sprintf(buf,"The Decrypted text is \"%s\"\n",output_text);
+    sprintf(buf, "\nThe Decrypted text is \"%s\"\n", output_text);
     info(0, buf);
   }
   return output_text;
 }
 
-int main(int argc, char **argv) {
-  
+int main(int argc, char **argv)
+{
+
   // set up wheels
-  wheels[0] = makeWheel("bcagdefhilkjomnrqpu vstwzyx.94/3,20!\\?@81\"5'+$(6)-7",letter_limit);
-  wheels[1] = makeWheel("chtzwefdbyiqljuvskgaxorpnm\"6-(1$873,04 /.!25'\\+?)9@",letter_limit);
-  wheels[2] = makeWheel("x6pr8g7+2!n0$dw\\z?@4lhya5mo.v)9-,1 (3sqiu'etb\"jcfk/",letter_limit);
-  wheels[3] = makeWheel("j\"kbcefpl?/,v6gw(2!0o.5yamh1 -7r3s8x)9u$i+t\\z'qdn4@",letter_limit);
+  wheels[0] = makeWheel("bcagdefhilkjomnrqpu vstwzyx.94/3,20!\\?@81\"5'+$(6)-7", letter_limit);
+  wheels[1] = makeWheel("chtzwefdbyiqljuvskgaxorpnm\"6-(1$873,04 /.!25'\\+?)9@", letter_limit);
+  wheels[2] = makeWheel("x6pr8g7+2!n0$dw\\z?@4lhya5mo.v)9-,1 (3sqiu'etb\"jcfk/", letter_limit);
+  wheels[3] = makeWheel("j\"kbcefpl?/,v6gw(2!0o.5yamh1 -7r3s8x)9u$i+t\\z'qdn4@", letter_limit);
+
+  // f.one = 'A';
+  // f.two = 'A';
+  // f.three = 'A';
+  // f.four = 'A';
+  // f.debug = 1;
+
+  // char *in = strdup("xUxa");// tem que dar dup porque a string é const
+  // char *out = Encryptor(in,4);
+
+  // printf("Encrypt:\n< %s\n> %s\n",in,out);
+
+  // in = strdup("@u14");
+  // out = Decryptor(in,4);
+
+  // printf("Decrypt:\n< %s\n> %s\n",in,out);
+
+  if (argc < 3)
+  {
+    printf("usage:\n%s <POSITION> <OPERATION> [<DEBUG>]\n", argv[0]);
+    printf("POSITION: the 4-letter initial rotor position (ex: AAAA)\n");
+    printf("OPERATION: either enc (to encrypt) or dec (to decrypt)\n");
+    printf("DEBUG: (optional) pass either 1 to get debug messages or 0 to leave it alone\n");
+    return 1;
+  }
+
+  char *position = strdup(argv[1]);
+
+  if (strlen(position) < 4)
+  {
+    printf("You must provide 4 values tor the rotors (ex: AAAA)\n");
+    return 1;
+  }
+
+  // let's set up the rotors
+  f.one = position[0];
+  f.two = position[1];
+  f.three = position[2];
+  f.four = position[3];
+
+  int isenc = 1; // i'll assume encrypt
+  if (!strcmp("dec", argv[2]))
+    isenc = 0;
+
+  f.debug = 0;
+  if (argc > 3)
+    f.debug = atoi(argv[3]);
   
-  // teste TODO implementar a interface de linha de comando que nem na versão do nodejs
-  
-  f.one = 'A';
-  f.two = 'A';
-  f.three = 'A';
-  f.four = 'A';
-  f.debug = 1;
-  
-  
-  char *in = strdup("xUxa");// tem que dar dup porque a string é const
-  char *out = Encryptor(in,4);
-  
-  printf("Encrypt:\n< %s\n> %s\n",in,out);
-  
-  in = strdup("@u14");
-  out = Decryptor(in,4);
-  
-  printf("Decrypt:\n< %s\n> %s\n",in,out);
-  
+  char *line = NULL;
+  size_t len; // how many chars we've read
+
+  printf("provide the message to %s:\n< ", isenc ? "encrypt" : "decrypt");
+
+  // this is posix, should work everywhere!
+  getline(&line,&len,stdin);
+
+  if(isenc)
+    printf("> %s\n",Encryptor(line,strlen(line)));
+  else
+    printf("> %s\n",Decryptor(line,strlen(line)));
+
   return 0;
 }
